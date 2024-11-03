4aa15c829288e67b92c3ff608caa1f18ef347d5d
RobbinYourCode <62479629+RobbinYourCode@users.noreply.github.com>
2024-11-03 01:34:40 -0400

    Final Web app for more logical functionality

--- web.py
+++ web.py
@@ -1,24 +1,33 @@
 import streamlit as sl
 import functions
 
-def add_todo():
-    todo = sl.session_state["new_todo"] + '\n'
-    todoList.append(todo)
-    functions.write_todoList(todoList)
-
-todoList = functions.read_todoList()
-
-sl.title("My Todo App")
+#Title and description section
+sl.title("Todo Web Application")
 sl.subheader("This is my todo app!")
 sl.write("This app is to increase your productivity and to keep track of your progress!")
 
-for index, todo in enumerate(todoList):
-    checkbox = sl.checkbox(todo, key=todo)
-    if checkbox:
-        todoList.pop(index)
-        functions.write_todoList(todoList)
-        del sl.session_state[todo]
-        sl.rerun()
+#Displays the todo list into the session if it isn't already present
+if "todoList" not in sl.session_state:
+    sl.session_state["todoList"] = functions.read_todoList()
 
+#Adds a todo at the end of the list and updates the session
+def add_todo():
+    sl.session_state["todoList"].append(sl.session_state["new_todo"] + '\n')
+    functions.write_todoList(sl.session_state["todoList"])
+    #Resets the input text field to an empty default state for user convenience
+    sl.session_state["new_todo"] = ""
+
+#Removes a todo via list index and updates the session accordingly
+def remove_todo(index):
+    sl.session_state["todoList"].pop(index)
+    functions.write_todoList(sl.session_state["todoList"])
+
+#In the event that a checkbox is checked or "True" in boolean, it calls the remove_todo method
+for index, todo in enumerate(sl.session_state["todoList"]):
+    #This pinpoints the exact index of the checkbox which each have their own unique ID
+    if sl.checkbox(todo, key=f"todo_{index}"):
+        remove_todo(index)
+
+#The input text field with all of its properties
 sl.text_input(label=" ", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
